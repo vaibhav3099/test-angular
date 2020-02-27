df125fa4f124c22b2ddb2621b63ba8e9742c9f34
vaibhav3099 <vj301999vj@gmail.com>
2020-02-28 00:51:42 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  imageUrl: string="assets/photo.jpeg";
   constructor() { }
 
   ngOnInit() {
